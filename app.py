8242b5adb9be22dc3fe20528f5c02be87f977234
Andres Mota - Laptop <andres.mt0611@gmail.com>
2023-12-21 18:06:59 -0400

    - Fixed Iternal Server Error 500 due template path for index and addExpenses html pages. - Fixed immutableMultiDict error on expDesc / request.form('Description') / request.form.get('Description'). CURRENT: Passed form value for 'Description' is giving value of 'None" despite form being properly completed. NEXT STEPS: 1. Add open/write csv file for input capture: --> Pass categories from form list. --> Write first three records and validate CSV.

--- app.py
+++ app.py
@@ -59,18 +59,9 @@ def index():
     total_cost = 0
     # for i in my_expenses:
     #     total_cost += float(i["cost"])
-    expensesByCategory = [
-        ("snacks", 1500.0),
-        ("restaurant", 25000.0),
-        ("services", 950.0),
-        ("clothes", 8000.0),
-        ("souvenirs", 3000.0),
-        ("hobbies", 8850.68),
-        ("transportation", 3251.37),
-        ("entertainment", 2750.0),
-        ("ATM", 30000.0),
-        ("miscellaneous", 5427.45),
-    ]
+    expensesByCategory = [        ("snacks", 1500.0),        ("restaurant", 25000.0),        ("services", 950.0),        
+                          ("clothes", 8000.0),        ("souvenirs", 3000.0),        ("hobbies", 8850.68),
+                          ("transportation", 3251.37),        ("entertainment", 2750.0),        ("ATM", 30000.0),        ("miscellaneous", 5427.45)  ]
     for i in expensesByCategory:
         total_cost += i[1]
 
